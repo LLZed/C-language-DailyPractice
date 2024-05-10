6601989e8a149119d572bcbe67bb0eadb612bd27
LLZed <145909255+LLZed@users.noreply.github.com>
2024-05-11 02:24:05 +0800

    Create text09.c

---
+++ OJ_Online_Programming_Common_IO_Exercises/text09.c
@@ -0,0 +1,40 @@
+/*
+对输入的字符串进行排序后输出
+打开以下链接可以查看正确的代码
+https://ac.nowcoder.com/acm/contest/5657#question
+
+输入描述：
+多个测试用例，每个测试用例一行。
+
+每行通过空格隔开，有n个字符，n＜100
+输出描述：
+对于每组测试用例，输出一行排序过的字符串，每个字符串通过空格隔开
+示例1
+输入例子：
+a c bb
+f dddd
+nowcoder
+输出例子：
+a bb c
+dddd f
+nowcoder
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+ 
+int main(){
+    char str[100][10];
+    int num = 0;
+    while(scanf("%s",str[num++]) != EOF){
+        if(getchar() == '\n'){
+            qsort(str,num,10,strcmp);
+            for(int i = 0;i < num;i++) 
+                printf("%s ",str[i]);
+            printf("\n");
+            num = 0; 
+        }
+    }
+    return 0;
+}
